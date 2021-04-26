954819863fb84ffef102d286cdc00057d121ccfc
Peter Lukacs <lukacs.peter.andras@gmail.com>
2021-04-26 14:22:34 +0000

    ✨ Add time of scraping and date published to data

--- src/main.py
+++ src/main.py
@@ -1,12 +1,25 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+from datetime import datetime
+from pytz import timezone
+import dateparser
+import re
 
 
 URL = "https://www.eumet.hu/vizhomerseklet/"
 page = requests.get(URL)
 
+time_of_scraping = datetime.now(timezone("CET")).strftime("%Y-%m-%d %H:%M:%S")
+
 soup = BeautifulSoup(page.content, "html.parser")
+date_published_text_hun = soup.find(
+    "p", text=re.compile("Kiadva.*")
+).get_text()
+date_published_hun = re.search(
+    "\\d{1,4}\\. .+ \\d{1,2}\\.", date_published_text_hun
+).group()
+date_pubished = dateparser.parse(date_published_hun).strftime("%Y-%m-%d")
 tables = soup.find_all("table", {"class": "sportho"})
 dfs = pd.read_html(URL, flavor="bs4", header=0, index_col=0)[:11]
 df_names_html = soup.find_all("strong", {"class": "orszagnev"})
@@ -14,9 +27,13 @@ df_names = map(lambda x: BeautifulSoup.get_text(x).capitalize(), df_names_html)
 for df_, water_name in zip(dfs, df_names):
     df_["name_of_water"] = water_name
 
-# add name of water
-# add scraping timestamp
-# add last website refresh date ("Kiadva")
+df_concat = pd.concat(dfs)
+df_concat["time_of_scraping_utc"] = time_of_scraping
+df_concat["date_published"] = date_pubished
+
 # Rename columns
 # Column values should only include numbers
+# Testing
+# Refactor ffs
 # Add logging
+# black format linebreak at 79
