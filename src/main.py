266bb3f87282476619bc49a2ff8e5150b1a7cd24
Peter Lukacs <lukacs.peter.andras@gmail.com>
2021-04-26 12:50:45 +0000

    ✨ Add name of water to scraped data

--- src/main.py
+++ src/main.py
@@ -7,5 +7,16 @@ URL = "https://www.eumet.hu/vizhomerseklet/"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
-tables = soup.find_all("table", class_="sportho")
-dfs = pd.read_html(URL, flavor="bs4", header=0, index_col=0)
+tables = soup.find_all("table", {"class": "sportho"})
+dfs = pd.read_html(URL, flavor="bs4", header=0, index_col=0)[:11]
+df_names_html = soup.find_all("strong", {"class": "orszagnev"})
+df_names = map(lambda x: BeautifulSoup.get_text(x).capitalize(), df_names_html)
+for df_, water_name in zip(dfs, df_names):
+    df_["name_of_water"] = water_name
+
+# add name of water
+# add scraping timestamp
+# add last website refresh date ("Kiadva")
+# Rename columns
+# Column values should only include numbers
+# Add logging
