6ae81e50b5a110f8ab36686ddc782be912494b30
Peter Lukacs <lukacs.peter.andras@gmail.com>
2021-04-30 11:57:06 +0000

    ✨ 🚧 Connect to db from Python

--- src/functions/temp_access_pg_db.py
+++ src/functions/temp_access_pg_db.py
@@ -1,11 +1,23 @@
 import pandas as pd
 from sqlalchemy import create_engine
+import os
 
-alchemyEngine = create_engine("postgresql+psycopg2://postgres@localhost:5432/postgres")
 
-dbConnection = alchemyEngine.connect()
+# Connecto to the database
+db_string = "postgresql://{}:{}@{}:{}/{}".format(
+    os.getenv("PG_DB_NAME"),
+    os.getenv("PG_PASSWORD"),
+    os.getenv("PG_HOST_NAME"),
+    os.getenv("PG_PORT"),
+    os.getenv("PG_USER_NAME"),
+)
 
-foo = pd.read_sql("select * from re_data", dbConnection)
-bar = pd.read_sql("select * from re_data_cleaned", dbConnection)
+db = create_engine(db_string)
 
-dbConnection.close()
+dbConnection = db.connect()
+
+dummy_table = pd.DataFrame({"col_1": [1, 2, 3], "col_2": ["a", "b", "c"]})
+
+dummy_table.to_sql("dummy_table", dbConnection, index=False, if_exists="replace")
+
+pd.read_sql("select * from dummy_table", dbConnection)
