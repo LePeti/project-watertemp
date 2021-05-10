c70259001218eed16c71f2a05e9735188b502fd3
Peter Lukacs <lukacs.peter.andras@gmail.com>
2021-05-10 16:44:27 +0000

    ⚡️ Convert water temp unique to an incremental table

--- dbt/models/water_temp_unique.sql
+++ dbt/models/water_temp_unique.sql
@@ -1,2 +1,11 @@
+{{
+    config(
+        materialized='incremental'
+    )
+}}
+
 SELECT DISTINCT *
 FROM {{ source('public', 'water_temp_raw') }}
+{% if is_incremental() %}
+  WHERE date_published > (SELECT max(date_published) FROM {{ this }})
+{% endif %}
