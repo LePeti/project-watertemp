2ad47507443d8f4f8ec0577fd692834b29553291
Peter Lukacs <lukacs.peter.andras@gmail.com>
2021-05-22 06:59:07 +0000

    🐛 Fix uniquing the water temp unique table
    It doesn't need to be unique an all raw columns. Only one time of scraping needed for each date_published.

--- dbt/models/water_temp_unique.sql
+++ dbt/models/water_temp_unique.sql
@@ -4,8 +4,21 @@
     )
 }}
 
-SELECT DISTINCT *
+SELECT
+  location,
+  water_depth_cm,
+  water_temp_celsius,
+  air_temp_celsius,
+  name_of_water,
+  MAX(time_of_scraping_cet) AS time_of_scraping_cet,
+  date_published
 FROM {{ source('public', 'water_temp_raw') }}
 {% if is_incremental() %}
   WHERE date_published > (SELECT max(date_published) FROM {{ this }})
 {% endif %}
+GROUP BY location,
+  water_depth_cm,
+  water_temp_celsius,
+  air_temp_celsius,
+  name_of_water,
+  date_published
