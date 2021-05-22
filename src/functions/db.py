b425d5b2d097a1541e2ed67606b1ab8ed05ad8d2
Peter Lukacs <lukacs.peter.andras@gmail.com>
2021-05-22 06:59:49 +0000

    ✨ Add heroku connection string concatenating function

--- src/functions/db.py
+++ src/functions/db.py
@@ -8,3 +8,12 @@ def concat_local_conn_string():
         f"{os.getenv('PG_HOST_NAME')}:{os.getenv('PG_PORT')}/"
         f"{os.getenv('PG_DB_NAME')}"
     )
+
+
+def concat_heroku_conn_string():
+    return (
+        f"postgresql+psycopg2://"
+        f"{os.getenv('HEROKU_PG_USER_NAME')}:{os.getenv('HEROKU_PG_PASSWORD')}@"
+        f"{os.getenv('HEROKU_PG_HOST_NAME')}:{os.getenv('HEROKU_PG_PORT')}/"
+        f"{os.getenv('HEROKU_PG_DB_NAME')}"
+    )
