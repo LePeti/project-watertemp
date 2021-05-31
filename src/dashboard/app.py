4812737960e02b8136b7e42fa1be037bc15c324b
Peter Lukacs <lukacs.peter.andras@gmail.com>
2021-05-31 12:47:16 +0000

    Setting dash debug to false

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -118,4 +118,4 @@ def populate_location_dropdown_values(name_of_water):
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True, host="0.0.0.0", port=port)
+    app.run_server(debug=False, host="0.0.0.0", port=port)
