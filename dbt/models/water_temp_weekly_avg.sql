9a6a7207df5e31a6326a7f1ec8ff64ab10984b60
Peter Lukacs <lukacs.peter.andras@gmail.com>
2021-10-26 07:27:29 +0000

    🗃️ Create new weekly aggregate table This will enable deletion from the raw and unique table

---
+++ dbt/models/water_temp_weekly_avg.sql
@@ -0,0 +1,19 @@
+{{
+    config(
+        materialized='incremental'
+    )
+}}
+
+SELECT
+  location,
+  name_of_water,
+  DATE_TRUNC('week', date_published::TIMESTAMP)::DATE AS date_published_weekly,
+  AVG(water_depth_cm::REAL) AS avg_water_depth_cm,
+  AVG(water_temp_celsius::REAL) AS avg_water_temp_celsius,
+  AVG(air_temp_celsius::REAL) AS avg_air_temp_celsius
+FROM {{ ref('water_temp_unique') }}
+{% if is_incremental() %}
+  WHERE date_published > (SELECT max(date_published) FROM {{ this }})
+{% endif %}
+GROUP BY 1, 2, 3
+ORDER BY 3 DESC, 1, 2
