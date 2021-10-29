85388c1fc31ac0319e4371346f02b6857856cadb
Peter Lukacs <lukacs.peter.andras@gmail.com>
2021-10-29 06:31:37 +0000

    🐛 Fix incremental model

--- dbt/models/water_temp_weekly_avg.sql
+++ dbt/models/water_temp_weekly_avg.sql
@@ -1,6 +1,7 @@
 {{
     config(
         materialized='incremental',
+        unique_key='unique_key',
         post_hook=[
           "DELETE FROM {{ ref('water_temp_unique') }}
            WHERE date_published::DATE < (
@@ -16,10 +17,11 @@ SELECT
   DATE_TRUNC('week', date_published::TIMESTAMP)::DATE AS date_published_weekly,
   AVG(water_depth_cm::REAL) AS avg_water_depth_cm,
   AVG(water_temp_celsius::REAL) AS avg_water_temp_celsius,
-  AVG(air_temp_celsius::REAL) AS avg_air_temp_celsius
+  AVG(air_temp_celsius::REAL) AS avg_air_temp_celsius,
+  location || '-' || name_of_water || '-' || DATE_TRUNC('week', date_published::TIMESTAMP)::DATE AS unique_key
 FROM {{ ref('water_temp_unique') }}
 {% if is_incremental() %}
-  WHERE date_published::DATE > (SELECT max(date_published_weekly) FROM {{ this }})
+  WHERE date_published::DATE >= (SELECT max(date_published_weekly) FROM {{ this }})
 {% endif %}
 GROUP BY 1, 2, 3
 ORDER BY 3 DESC, 1, 2
