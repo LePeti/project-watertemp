544f0e6a95c7ac5da02476e22ee3cd99939744f4
Peter Lukacs <lukacs.peter.andras@gmail.com>
2021-10-31 07:58:06 +0000

    🗃️ Truncate uniqued data after 2 months

--- dbt/models/water_temp_weekly_avg.sql
+++ dbt/models/water_temp_weekly_avg.sql
@@ -5,23 +5,23 @@
         post_hook=[
           "DELETE FROM {{ ref('water_temp_unique') }}
            WHERE date_published::DATE < (
-             SELECT MAX(date_published::DATE) FROM {{ ref('water_temp_unique') }}
-           ) - INTERVAL '2 YEAR';"
+               SELECT MAX(date_published::DATE) FROM {{ ref('water_temp_unique') }}
+           ) - INTERVAL '2 MONTHS';"
         ]
     )
 }}
 
 SELECT
-  location,
-  name_of_water,
-  DATE_TRUNC('week', date_published::TIMESTAMP)::DATE AS date_published_weekly,
-  AVG(water_depth_cm::REAL) AS avg_water_depth_cm,
-  AVG(water_temp_celsius::REAL) AS avg_water_temp_celsius,
-  AVG(air_temp_celsius::REAL) AS avg_air_temp_celsius,
-  location || '-' || name_of_water || '-' || DATE_TRUNC('week', date_published::TIMESTAMP)::DATE AS unique_key
+    location,
+    name_of_water,
+    DATE_TRUNC('week', date_published::TIMESTAMP)::DATE AS date_published_weekly,
+    AVG(water_depth_cm::REAL) AS avg_water_depth_cm,
+    AVG(water_temp_celsius::REAL) AS avg_water_temp_celsius,
+    AVG(air_temp_celsius::REAL) AS avg_air_temp_celsius,
+    location || '-' || name_of_water || '-' || DATE_TRUNC('week', date_published::TIMESTAMP)::DATE AS unique_key
 FROM {{ ref('water_temp_unique') }}
 {% if is_incremental() %}
-  WHERE date_published::DATE >= (SELECT max(date_published_weekly) FROM {{ this }})
+    WHERE date_published::DATE >= (SELECT max(date_published_weekly) FROM {{ this }})
 {% endif %}
 GROUP BY 1, 2, 3
 ORDER BY 3 DESC, 1, 2
