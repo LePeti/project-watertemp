b096167ff9372596bf14b302515bc33c9cf014e1
Peter Lukacs <lukacs.peter.andras@gmail.com>
2021-10-31 07:55:54 +0000

    🗃️ Truncate raw data after 2 weeks

--- dbt/models/water_temp_unique.sql
+++ dbt/models/water_temp_unique.sql
@@ -5,26 +5,25 @@
           "DELETE FROM {{ source('public', 'water_temp_raw') }}
            WHERE date_published::DATE < (
              SELECT MAX(date_published::DATE) FROM {{ source('public', 'water_temp_raw') }}
-           ) - INTERVAL '2 MONTH';"
+           ) - INTERVAL '2 WEEKS';"
         ]
     )
 }}
 
-SELECT
-  location,
-  water_depth_cm,
-  water_temp_celsius,
-  air_temp_celsius,
-  name_of_water,
-  MAX(time_of_scraping_cet) AS time_of_scraping_cet,
-  date_published
+SELECT location,
+    water_depth_cm,
+    water_temp_celsius,
+    air_temp_celsius,
+    name_of_water,
+    MAX(time_of_scraping_cet) AS time_of_scraping_cet,
+    date_published
 FROM {{ source('public', 'water_temp_raw') }}
 {% if is_incremental() %}
-  WHERE date_published > (SELECT max(date_published) FROM {{ this }})
+    WHERE date_published > (SELECT max(date_published) FROM {{ this }})
 {% endif %}
 GROUP BY location,
-  water_depth_cm,
-  water_temp_celsius,
-  air_temp_celsius,
-  name_of_water,
-  date_published
+    water_depth_cm,
+    water_temp_celsius,
+    air_temp_celsius,
+    name_of_water,
+    date_published
