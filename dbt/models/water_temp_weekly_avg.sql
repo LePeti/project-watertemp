05cc63945d5acecd0abfa8dd6fc4b492667ecc9a
Peter Lukacs <lukacs.peter.andras@gmail.com>
2021-10-31 08:48:44 +0000

    🗃️ Truncate Hungarian lakes only after 1 year
    Truncate the rest after 1 month

--- dbt/models/water_temp_weekly_avg.sql
+++ dbt/models/water_temp_weekly_avg.sql
@@ -3,10 +3,18 @@
         materialized='incremental',
         unique_key='unique_key',
         post_hook=[
-          "DELETE FROM {{ ref('water_temp_unique') }}
-           WHERE date_published::DATE < (
-               SELECT MAX(date_published::DATE) FROM {{ ref('water_temp_unique') }}
-           ) - INTERVAL '2 MONTHS';"
+            "DELETE FROM {{ ref('water_temp_unique') }}
+            WHERE (
+                date_published::DATE < (
+                    SELECT MAX(date_published::DATE) FROM {{ ref('water_temp_unique') }}
+                ) - INTERVAL '1 MONTHS'
+                AND name_of_water <> 'Magyar tavak'
+            ) OR (
+                date_published::DATE < (
+                    SELECT MAX(date_published::DATE) FROM {{ ref('water_temp_unique') }}
+                ) - INTERVAL '12 MONTHS'
+                AND name_of_water = 'Magyar tavak'
+            );"
         ]
     )
 }}
