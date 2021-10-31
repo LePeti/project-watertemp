81a568512f4b63e3330b22cb3d2ceec93f9de498
Peter Lukacs <lukacs.peter.andras@gmail.com>
2021-10-31 10:58:40 +0000

    🗃️ Rename columns in `table_stats` table

--- dbt/models/ops/table_stats.sql
+++ dbt/models/ops/table_stats.sql
@@ -1,15 +1,17 @@
 {{
     config(
         materialized='incremental',
-        unique_key='date || relname'
+        unique_key='date || table_name'
     )
 }}
 
-SELECT 
-    CURRENT_DATE AS date,
-    relname,
-    n_live_tup
+SELECT CURRENT_DATE AS date,
+    relname AS table_name,
+    n_live_tup AS row_count
 FROM {{ source('pg_catalog', 'pg_stat_user_tables') }}
 WHERE schemaname = 'public'
-    AND (relname LIKE 'water_temp_%' OR relname LIKE 'table_stats')
+    AND (
+        relname LIKE 'water_temp_%'
+        OR relname LIKE 'table_stats'
+    )
 ORDER BY relname
