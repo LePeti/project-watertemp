2569f1fbcc0891e6b6508b86ef577954d37f26b1
Peter Lukacs <lukacs.peter.andras@gmail.com>
2025-05-10 13:32:02 +0000

    Adapt conn string to moving to GCP

--- src/functions/db.py
+++ src/functions/db.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 
 
 def query_water_temps_unique_heroku():
-    db_string = concat_heroku_conn_string()
+    db_string = concat_gcp_conn_string()
     engine = create_engine(db_string)
     query_string = (
         f"SELECT * FROM {os.getenv('HEROKU_PG_DB_NAME')}.public.water_temp_unique"
@@ -21,7 +21,9 @@ def query_water_temps_unique():
     with engine.connect() as con:
         water_temps = pd.read_sql(query_string, con)
     water_temps["water_temp_celsius"] = pd.to_numeric(water_temps["water_temp_celsius"])
-    water_temps["date_published"] = pd.to_datetime(water_temps["date_published"]).dt.date
+    water_temps["date_published"] = pd.to_datetime(
+        water_temps["date_published"]
+    ).dt.date
     return water_temps
 
 
@@ -45,10 +47,10 @@ def concat_conn_string():
     )
 
 
-def concat_heroku_conn_string():
+def concat_gcp_conn_string():
     return (
         f"postgresql+psycopg2://"
-        f"{os.getenv('HEROKU_PG_USER_NAME')}:{os.getenv('HEROKU_PG_PASSWORD')}@"
-        f"{os.getenv('HEROKU_PG_HOST_NAME')}:{os.getenv('HEROKU_PG_PORT')}/"
-        f"{os.getenv('HEROKU_PG_DB_NAME')}"
+        f"{os.getenv('GCP_PG_USER_NAME')}:{os.getenv('GCP_PG_PASSWORD')}@"
+        f"{os.getenv('GCP_PG_HOST_NAME')}:{os.getenv('GCP_PG_PORT')}/"
+        f"{os.getenv('GCP_PG_DB_NAME')}"
     )
